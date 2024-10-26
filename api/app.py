d44bc1faca5635d7cb2361cda54f75cf574e92ae
codeblue12 <yasir.codeblue@gmail.com>
2024-10-26 12:33:38 +0500

    without cloud

--- api/app.py
+++ api/app.py
@@ -1,38 +1,14 @@
-import cloudinary
-import cloudinary.uploader
 from flask import Flask, request, jsonify
 from playwright.sync_api import sync_playwright
 import time
-from _playwright import close_playwright,init_playwright
 
-# Configure Cloudinary
-cloudinary.config(
-    cloud_name='dsfr7nm3a',
-    api_key='914261393664548',
-    api_secret='7uckXI5naaQOjW8xnQ_G34YrRB0'
-)
+# Configure Imgbb
 
 app = Flask(__name__)
 
-def upload_to_cloudinary(image_bytes):
-    """Upload an image to Cloudinary"""
-    try:
-        result = cloudinary.uploader.upload(
-            image_bytes,
-            resource_type="image",
-            upload_preset="pinterest"  # Adjust to your preset
-        )
-        print(f"Screenshot uploaded to Cloudinary: {result['secure_url']}")
-        return result['secure_url']
-    except Exception as error:
-        print(f"Error uploading to Cloudinary: {error}")
-        raise
-
-import time
-
 def take_screenshot_and_upload():
-    """Automate steps to take a screenshot, then upload to Cloudinary."""
-    playwright = init_playwright()
+    """Automate steps to take a screenshot, then upload to Imgbb."""
+    playwright = sync_playwright().start()
     browser = None
     try:
         browser = playwright.chromium.launch(headless=True)
@@ -62,10 +38,10 @@ def take_screenshot_and_upload():
         # Take screenshot of the specified element
         time.sleep(2)  # Add delay if necessary
         screenshot_bytes = screenshot_elem.screenshot(type="png")
-
-        # Upload screenshot to Cloudinary
-        screenshot_url = upload_to_cloudinary(screenshot_bytes)
-        return {"Screenshot URL": screenshot_url}
+        print("captured")
+        # # Upload screenshot to Imgbb
+        # screenshot_url = upload_to_imgbb(screenshot_bytes)
+        # return {"Screenshot URL": screenshot_url}
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -74,7 +50,7 @@ def take_screenshot_and_upload():
     finally:
         if browser:
             browser.close()
-        close_playwright(playwright)
+        playwright.stop()
 
 @app.route('/api/hey', methods=['GET'])
 def home():
